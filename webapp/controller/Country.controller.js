fe4cbee2fbd565e69f63b61ecebde90e580573a1
Pjotrs Gojs <pjotrsgojs@gmail.com>
2019-10-03 07:59:50 +0000

    Added Country(contr,view) Change manifest

---
+++ webapp/controller/Country.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+		"./BaseController",
+		'jquery.sap.global',
+		'sap/m/MessageToast',
+		'sap/ui/core/mvc/Controller',
+		'sap/ui/model/json/JSONModel'
+	], function(BaseController, jQuery, MessageToast, Controller, JSONModel) {
+	"use strict";
+
+		return BaseController.extend("com.BandB.BandB.controller.Country", {
+
+		onInit : function() {
+		}
+
+	});
+});
